d6baaa357200e9c1072d564a9f9917ede822c52f
hafsaamir-prog <hafsaamir0093@gmail.com>
2026-07-02 18:00:57 +0500

    Added menu button and links

--- src/App.jsx
+++ src/App.jsx
@@ -15,4 +15,13 @@ export default function App() {
                 <button className="menu-button" onClick={()=>setIsMenuOpen(!isMenuOpen)}>
                     {isMenuOpen ? 'X': '☰'}
                 </button>
-                
+                <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#about">About</a></li>
+                </ul>
+
+            </nav>
+            <div className="hero-content">
+                <div className="main-logo-image"></div>
+            </div>
