6000352f0926d4c5089b97a717245bd76026a654
hafsaamir-prog <hafsaamir0093@gmail.com>
2026-07-02 17:03:37 +0500

    Added a navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
 import React, {useState} from 'react';
+import './App.css';
+export default function App() {
+    const {isMenuOpen, SetIsMenuOpen} = useState(false);
+    return(
+        <div className="hero-contatainer">
+            <nav className= "navbar">
+                <div className="logo-section">
+                    <div className="gold-circle"></div>
+                    <div className = "brand-text">
+                        <span className="brand-title">TGSJ</span>
+                        <span classname="brand-subtitle">THEMIS GLOBAL SCAN JUSTICE</span>
+                    </div>    
+                </div>
+                <button className="menu-button" onClick={()=>setIsMenuOpen(!isMenuOpen)}>
+                    {isMenuOpen ? 'X': '☰'}
+                </button>
+                
