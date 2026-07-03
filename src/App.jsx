7adaba62b1d756a5d6eb5012d4772bce2abee1bb
hafsaamir-prog <hafsaamir0093@gmail.com>
2026-07-03 10:59:06 +0500

    Fixed the errors in the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React, {useState} from 'react';
 import './App.css';
 export default function App() {
-    const {isMenuOpen, SetIsMenuOpen} = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     return(
-        <div className="hero-contatainer">
+        <div className="hero-container">
             <nav className= "navbar">
                 <div className="logo-section">
                     <div className="gold-circle"></div>
                     <div className = "brand-text">
                         <span className="brand-title">TGSJ</span>
-                        <span classname="brand-subtitle">THEMIS GLOBAL SCAN JUSTICE</span>
+                        <span className="brand-subtitle">THEMIS GLOBAL SCAN JUSTICE</span>
                     </div>    
                 </div>
                 <button className="menu-button" onClick={()=>setIsMenuOpen(!isMenuOpen)}>
