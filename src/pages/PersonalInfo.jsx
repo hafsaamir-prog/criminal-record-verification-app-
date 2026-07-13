a1bce3e9eb876bbebe32cc05b7d911569a92af26
hafsaamir-prog <hafsaamir0093@gmail.com>
2026-07-13 17:55:47 +0500

    Add Verification Form pages

---
+++ src/pages/PersonalInfo.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import "./PersonalInfo.css";
+
+export default function PersonalInfo(){
+    return(
+        <form className="personal-info-form" onSubmit ={handleSubmit}>
+            <h2 className ="form-title">Personal Information </h2>
+            <p className ="form-subtitle"> 
+                Please complete before begining the verification process.
+            </p> 
+
+            <div className ="form-group">
+                <label> Father's Full Name *</label>
+                <input 
+                    type="text"
+                    name="fatherName"
+                    value={formData.fatherName}
+                    onChange={handleChange}
+                    placeholder="Robert Smith"                
+                    required/>
+            </div>
+
+            <div className="form-group">
+                <label>Mother's Full Name *</label>
+                <input
+                type="text"
+                name="motherName"
+                value={formData.motherName}
+                onChange={handleChange}
+                placeholder="Elizabeth Smith"
+                required />
+            </div>
+
+            <div className ="form-group">
+                <label>Mother's Maiden Name *</label>
+                <input 
+                type="text"
+                name="motherMaidenName"
+                value={formData.motherMaidenName}
+                onChange={handleChange}
+                placeholder="Johnson"
+                required/>
+            </div>
+
+            <div className ="form-group">
+                <label>E-mail Address *</label>
+                <div className ="verification-row">
+                    <input 
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        placeholder="john@example.com"
+                        required/>
+                    <button
+                    type="button"
+                    className="send-code-btn"
+                    onClick={()=> handleSendCode("email")}>
+                        Send Code 
+                    </button> 
+                </div>
+            </div>      
+
+            <div classNaame ="form-group">
+                <label>Phone Number *</label>
+                <div className ="verification-row">
+                    <input 
+                        type="tel"
+                        name="phone"
+                        value={formDta.phone}
+                        onChange={handleChange}
+                        placeholder="+ 92 313422680"
+                        required/>
+                    <button 
+                        type="button"
+                        className="send-code-btn"
+                        onClick={()=>handleSendCode("phone")}>
+                            Send Code
+                    </button>
+                </div>
+            </div>
+
+            <div className="form-group">
+                <label>Home Adress *</label>
+                <textarea 
+                    rows ="4"
+                    name= "homeAddres"
+                    value={formData.homeAddress}
+                    onChange={handleChange}
+                    placeholder="Enter your complete Home Address"
+                    required/>
+            </div>
+
+            <div className="form-group">
+                <label>Employer Email</label>
+                <input
+                    type="email"
+                    name="employerEmail"
+                    value={formData.employerEmail}
+                    onChange={handleChange}
+                    placeholder="hr@company.com"/>
+            </div>
+            <button
+                type="submit"
+                className="contibue-btn">
+                    Continue to Residence History → 
+                    </button>
+        </form>
+
+    );
+}
+s
