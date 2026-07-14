8b25730ba2da5fcde28d353b79c7dfad46f3b433
hafsaamir-prog <hafsaamir0093@gmail.com>
2026-07-14 14:03:09 +0500

    step 2 of veri form

---
+++ src/pages/ResidenceHistory.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./ResidenceHistory.css";
+
+export default function ResidenceHistory({
+    formData,
+    setFormDta,
+    nextStep,
+    prevStep}){
+    }
+    function handleCountryChange(index,e){
+        const{name,value=e.target;
+            const updatedCountries=[..formData.countries];
+            updatedCountries[index][name]=value;
+
+            setFormData({
+                ...formData,
+                countries:updatedCountries
+            });
+        }
+        function addCountry(){
+            setFormData({
+                ...formData,
+                countries:[
+                    ...formData.countries,
+                    {
+                        country:" ",
+                        fromYear:" ",
+                        toYear:" ",
+                        taxNumber:"",
+                        driverseLicense:"",
+                        nationalId:"",
+                        otherDocuments:" "
+                    }]
+
+                    }
+                    }
+                ]
+
+            })
+        }
+    }
+)}
